3c67d9cf5706595afa8dbc46e0cc22e733653e0a
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-23 12:06:31 -0600

    Add navigation to other screen with parameters

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -10,6 +10,10 @@ class MovieScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movieId),
+      ),
+    );
   }
 }
