faf5037c95a179b54808ae30835add2a449fba5d
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-23 11:27:03 -0600

    create movie screen

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class MovieScreen extends StatelessWidget {
+  static const String name = 'movie_screen';
+
+  final String movieId;
+
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
