cf9f74b490af16a2d414168451283bb6f3a3fac6
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-23 13:52:43 -0600

    make request

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cinemapedia/presentation/provider/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../domain/entities/movie.dart';
 
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const String name = 'movie_screen';
 
   final String movieId;
@@ -9,11 +13,37 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    //link to the provider to load the movie
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+   final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+   if(movie == null){
+     return const Scaffold(
+       body: Center(
+         child: CircularProgressIndicator(),
+       ),
+     );
+   }
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(movieId),
+        title: Text(widget.movieId),
       ),
     );
   }
+
 }
