2b153aec6649b9c7dbab5a774d1e81087b6f196b
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-23 17:20:30 -0600

    add movie info

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -38,7 +38,15 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomSliverAppBar(movie: movie)],
+      slivers: [
+        _CustomSliverAppBar(movie: movie),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _MovieDetails(
+                      movie: movie,
+                    ),
+                childCount: 1))
+      ],
     ));
   }
 }
@@ -77,23 +85,89 @@ class _CustomSliverAppBar extends StatelessWidget {
                   colors: [Colors.transparent, Colors.black87],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                  stops:  [0.9, 1.0]
-              ),
+                  stops: [0.9, 1.0]),
             )),
           ),
           const SizedBox.expand(
             child: DecoratedBox(
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Colors.black87,Colors.transparent, ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomCenter,
-                      stops:  [0.0, 0.3]
-                  ),
-                )),
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.black87,
+                    Colors.transparent,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomCenter,
+                  stops: [0.0, 0.3]),
+            )),
           )
         ]),
       ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Image to the movie
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              //Details of the movie
+              SizedBox(
+                width: (size.width -40) *.7,
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(movie.title, style: textStyles.titleLarge,),
+                    Text(movie.overview)
+                  ],
+                )
+
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child:Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Chip(
+                  label: Text(gender),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                ),
+              ))
+            ],
+          )
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+      ],
     );
   }
 }
