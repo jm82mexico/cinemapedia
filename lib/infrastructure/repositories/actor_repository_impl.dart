4539ce0d62b32918c042767198c08eac5ee28934
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-23 18:02:23 -0600

    actors data flow configuration

---
+++ lib/infrastructure/repositories/actor_repository_impl.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/domain/repositories/actors_repository.dart';
+
+class ActorRepositoryImpl extends ActorsRepository{
+
+  final ActorDatasource datasource;
+
+
+  ActorRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Actor>> getActorsByMovie(String movieId) {
+    return datasource.getActorsByMovie(movieId);
+  }
+
+}
