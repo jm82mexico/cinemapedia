6c2b5977562f6c056315082cf444ed0fc195cf57
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-24 17:21:35 -0600

    build suggestions

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,8 +1,8 @@
-
-
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../provider/providers.dart';
 
@@ -11,7 +11,6 @@ class CustomAppbar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleLarge;
 
@@ -20,21 +19,34 @@ class CustomAppbar extends ConsumerWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Row(
           children: [
-            Icon(Icons.movie_creation_outlined, color: colors.primary,),
+            Icon(
+              Icons.movie_creation_outlined,
+              color: colors.primary,
+            ),
             const SizedBox(width: 5),
-            Text('Cinemapedia', style: titleStyle,),
+            Text(
+              'Cinemapedia',
+              style: titleStyle,
+            ),
             const Spacer(),
-            IconButton(onPressed: (){
-              final movieRepository = ref.read(movieRepositoryProvider);
-              showSearch(context: context, delegate: SearchMovieDelegate(
-                //Send only the reference to the searchMovies method
-                searchMovie: movieRepository.searchMovies
-              ));
-            }, icon: const Icon(Icons.search))
+            IconButton(
+                onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  showSearch<Movie?>(
+                          context: context,
+                          delegate: SearchMovieDelegate(
+                              //Send only the reference to the searchMovies method
+                              searchMovie: movieRepository.searchMovies))
+                      .then((movie) {
+                    if (movie != null) {
+                      context.push('/movie/${movie.id}');
+                    }
+                  });
+                },
+                icon: const Icon(Icons.search))
           ],
         ),
       ),
     );
   }
 }
-
