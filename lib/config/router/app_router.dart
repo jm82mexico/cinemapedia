9f4f20bacbcaf3d5c233e6ad1d3eb14e35e281d6
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-08-25 11:01:04 -0600

    implementar shellRoute

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,28 +1,54 @@
-
-
-
 import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen( childView: FavoritesView(),),
-      routes: [
-        GoRoute(
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const HomeView();
+        },
+        routes: [
+          GoRoute(
             path: 'movie/:id',
             name: MovieScreen.name,
             builder: (context, state) {
               final movieId = state.pathParameters['id'] ?? 'no-id';
               return MovieScreen(movieId: movieId);
             },
-        )
-      ],
-
-    )
-  ],
-);
+          )
+        ],
+      ),
+      GoRoute(
+        path: '/favorites',
+        builder: (context, state) {
+          return const FavoritesView();
+        },
+      )
+    ],
+  )
+]
+    // routes: [
+    //   GoRoute(
+    //     path: '/',
+    //     name: HomeScreen.name,
+    //     builder: (context, state) => const HomeScreen( childView: FavoritesView(),),
+    //     routes: [
+    //       GoRoute(
+    //           path: 'movie/:id',
+    //           name: MovieScreen.name,
+    //           builder: (context, state) {
+    //             final movieId = state.pathParameters['id'] ?? 'no-id';
+    //             return MovieScreen(movieId: movieId);
+    //           },
+    //       )
+    //     ],
+    //
+    //   )
+    // ],
+    );
