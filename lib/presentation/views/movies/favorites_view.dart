37fb72de5a7b77ef9cf5426a6d574121434ba9c3
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-09-02 17:35:20 -0600

    Mostrar peliculas favoritas

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,19 +1,39 @@
-
-
+import 'package:cinemapedia/presentation/provider/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+    final movies =
+        await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Favorites'),
+        ),
+        body: ListView.builder(
+          itemCount: favoriteMovies.length,
+          itemBuilder: (context, index) {
+            final movie = favoriteMovies[index];
+            return Text(movie.title);
+          },
+        ));
   }
 }
